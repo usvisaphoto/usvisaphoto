2a0da4138c3a725e44753bd61c38c7234426708a
usvisaphoto <test@gmail.com>
2026-07-20 12:17:41 +0900

    Add Google Analytics 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -47,6 +48,7 @@ export default function RootLayout({
         <Footer />
         <Analytics />
       </body>
+      <GoogleAnalytics gaId="G-2PM4FCHM8F" />
     </html>
   );
 }
